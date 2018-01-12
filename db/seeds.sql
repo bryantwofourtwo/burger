b40da5a1b34b6a643e43a5070031f8614ea9a299
bryantwofourtwo <bryantwofourtwo@gmail.com>
2018-01-12 16:09:49 -0700

    folders, files and db added

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Pepper Jack Bison Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Doubleshot Tequila Lime Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double-Trouble Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cali-Mayo Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Western Style Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple-Stacked Fried Egg Burger', false);
